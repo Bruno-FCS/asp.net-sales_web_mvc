9f288ce5894c7ad58e2e20eb952ec5329a4b530e
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-10-01 19:01:22 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,9 +17,21 @@ namespace SalesWebMvc.Services
             return _context.Seller.OrderBy(x => x.Name).ToList();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(seller => seller.Id == id);
+        }
+
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
